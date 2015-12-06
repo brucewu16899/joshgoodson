2989db83e9b9fa47ecd8c8524641d37fa7cda234
Josh Goodson <josh@joshgoodson.com>
2015-12-05 22:13:32 -0600

    User table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(UserTableSeeder::class);
         $this->call(TaskTableSeeder::class);
 
         $this->call(RoleTableSeeder::class);
